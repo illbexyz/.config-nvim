e7943201b64676384383dc4131da6fb39a3f7b5b
Alberto Nicoletti <alberto.nicoletti@musixmatch.com>
2024-08-27 18:53:17 +0200

    feat: use nvchad

---
+++ lua/plugins/init.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  --
+  {
+    "sindrets/diffview.nvim",
+    lazy = false
+  },
+
+  {
+    "L3MON4D3/LuaSnip",
+    enabled = false
+  }
+}
