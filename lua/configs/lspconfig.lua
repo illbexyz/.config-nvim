2ee659292998d5aa5af367f5204d3a97fceb9f22
Alberto Nicoletti <3688366+illbexyz@users.noreply.github.com>
2024-08-27 20:35:54 +0200

    feat: add lsps + format on save

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,8 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "tsserver", "eslint" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -16,9 +15,3 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
-lspconfig.tsserver.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-}
