273e326c1cd09866c9702deea56540134a03ad58
Alberto Nicoletti <3688366+illbexyz@users.noreply.github.com>
2024-08-28 01:02:28 +0200

    feat: add diffview

--- init.lua
+++ init.lua
@@ -796,6 +796,13 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
+  { -- Git Diffview
+    'sindrets/diffview.nvim',
+    opts = {
+      enhanced_diff_hl = true,
+    },
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
