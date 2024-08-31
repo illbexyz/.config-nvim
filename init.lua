af25ce4aa00f5ae067c4ca8c556059164cbe2346
Alberto Nicoletti <3688366+illbexyz@users.noreply.github.com>
2024-08-31 11:52:38 +0200

    feat: map diffview commands

--- init.lua
+++ init.lua
@@ -805,6 +805,10 @@ require('lazy').setup({
     opts = {
       enhanced_diff_hl = true,
     },
+    config = function()
+      vim.keymap.set('n', '<leader>do', ':DiffviewOpen<CR>', { desc = '[D]iff [O]ree' })
+      vim.keymap.set('n', '<leader>dc', ':DiffviewClose<CR>', { desc = '[D]iff [C]lose' })
+    end,
   },
 
   {
