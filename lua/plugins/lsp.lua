7c603a22c20cd07b1d90d140abfcd9458a41bf40
Alberto Nicoletti <illbexyz@users.noreply.github.com>
2025-12-18 10:01:19 +0100

    fix: rename tsserver to ts_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -147,9 +147,7 @@ return {
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         eslint = {},
         --
 
